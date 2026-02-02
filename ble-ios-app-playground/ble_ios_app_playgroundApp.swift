7d9ffe95c5049bf2e10e0f5a6f199f14699c820f
gabriel almendarez <gabriel.almendarez@butterflymx.com>
2026-02-02 14:43:30 -0800

    Initial Commit

---
+++ ble-ios-app-playground/ble_ios_app_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ble_ios_app_playgroundApp.swift
+//  ble-ios-app-playground
+//
+//  Created by gabriel almendarez on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ble_ios_app_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
